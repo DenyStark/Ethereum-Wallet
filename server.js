910d6b9ce42352076de147b5192c40c2fb8ebda9
DenyStark <denyskrolevetskyi@gmail.com>
2019-05-05 13:48:12 +0300

    Ref: app.js + server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-'use strict'
-
-const app = require('./app')
-const config = require('./config')
-const logger = require('./logger')
-const port = config.port || 3000
+const app = require('./app');
+const config = require('./config');
+const logger = require('./logger');
+const port = config.port || 3000;
 
 app.listen(port, () => {
-    logger.info(`Server is listening on port ${port}`)
-})
+  logger.info('Server is listening on port ' + port);
+});
