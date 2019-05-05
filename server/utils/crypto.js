6f006256f62cdd1adeaa443fd92ae371f73cc780
DenyStark <denyskrolevetskyi@gmail.com>
2019-05-05 14:07:45 +0300

    Ref: Utils > crypto

--- server/utils/crypto.js
+++ server/utils/crypto.js
@@ -1,35 +1,32 @@
-'use strict'
+const crypto = require('crypto');
+const config = require('../../config');
 
-const crypto = require('crypto')
-const config = require('../../config')
+const algorithm = 'aes-256-cbc';
+const key = config.encryptionKey;
 
-const IV_LENGTH = 16 // For AES, this is always 16
+const encrypt = (text) => {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
 
-function encrypt(text) {
-    const iv = crypto.randomBytes(IV_LENGTH)
-    const cipher = crypto.createCipheriv(
-        'aes-256-cbc', Buffer.from(config.encryptionKey), iv)
-    let encrypted = cipher.update(text)
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
 
-    encrypted = Buffer.concat([encrypted, cipher.final()])
+  return `${iv.toString('hex')}:${encrypted.toString('hex')}`;
+};
 
-    return `${iv.toString('hex')}:${encrypted.toString('hex')}`
-}
+const decrypt = (text) => {
+  const textParts = text.split(':');
+  const iv = Buffer.from(textParts.shift(), 'hex');
+  const encryptedText = Buffer.from(textParts.join(':'), 'hex');
+  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
 
-function decrypt(text) {
-    const textParts = text.split(':')
-    const iv = Buffer.from(textParts.shift(), 'hex')
-    const encryptedText = Buffer.from(textParts.join(':'), 'hex')
-    const decipher = crypto.createDecipheriv(
-        'aes-256-cbc', Buffer.from(config.encryptionKey), iv)
-    let decrypted = decipher.update(encryptedText)
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
 
-    decrypted = Buffer.concat([decrypted, decipher.final()])
+  return decrypted.toString();
+};
 
-    return decrypted.toString()
-}
-
-module.exports = { 
-    encrypt,
-    decrypt
-}
+module.exports = {
+  encrypt,
+  decrypt,
+};
