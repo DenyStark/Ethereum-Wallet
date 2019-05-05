c62f894ef457fef15215544ab2d3faa1880fb667
DenyStark <denyskrolevetskyi@gmail.com>
2019-05-05 19:31:01 +0300

    Feat: Utils > Web3 > isAddress

--- server/utils/web3.js
+++ server/utils/web3.js
@@ -2,13 +2,17 @@ const Web3 = require('web3');
 
 const web3 = new Web3();
 const accounts = web3.eth.accounts;
+const utils = web3.utils;
 
 const generateNewAccount = () => {
-  const account = accounts.create(web3.utils.randomHex(32));
+  const account = accounts.create(utils.randomHex(32));
   delete account.accounts;
   return account;
 };
 
+const isAddress = (address) => (utils.isAddress(address));
+
 module.exports = {
   generateNewAccount,
+  isAddress,
 };
