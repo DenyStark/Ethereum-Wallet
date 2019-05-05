3242ef45145e3f936f3cecbdff48c6f9fa947936
DenyStark <denyskrolevetskyi@gmail.com>
2019-05-05 20:12:48 +0300

    Tests: tests

--- test/utils/crypto.js
+++ test/utils/crypto.js
@@ -1,13 +1,11 @@
-'use strict'
-
-const { encrypt, decrypt } = require('../../server/utils/crypto')
-const { expect } = require('chai')
+const { encrypt, decrypt } = require('../../server/utils/crypto');
+const { expect } = require('chai');
 
 describe.skip('encrypt', () => {
-    it('should encrypt & decrypt undefined message', () => {
-        const msg = undefined
-        const encrypted = encrypt(msg)
-        const decrypted = decrypt(encrypted)
-        expect(decrypted).to.equal(msg)
-    })
-})
+  it('should encrypt & decrypt undefined message', () => {
+    const msg = undefined;
+    const encrypted = encrypt(msg);
+    const decrypted = decrypt(encrypted);
+    expect(decrypted).to.equal(msg);
+  });
+});
